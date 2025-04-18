93050c8a219d3d6c4aa9402f041584f077ff3387
kufx <144138627+kufx@users.noreply.github.com>
2025-04-19 00:27:59 +0800

    Update redirect.php

--- usr/themes/kunav/redirect.php
+++ usr/themes/kunav/redirect.php
@@ -1,6 +1,6 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
-<?php $this->need('head.php'); ?>
+
 
     <?php $target = urldecode($_GET['target']); ?>
  
