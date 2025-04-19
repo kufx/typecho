6e3226a0647f625dd1d08a501d1b47686c65d1a9
kufx <144138627+kufx@users.noreply.github.com>
2025-04-19 15:54:19 +0800

    Update page.php

--- usr/themes/kunav/page.php
+++ usr/themes/kunav/page.php
@@ -7,7 +7,38 @@
 
 <h1 class="site-title"><?php $this->title() ?></h1>
 
-<?php echo getContentTest($this->content); ?>                </div>
+<!-- 回复&登录可见 -->
+                <?php
+                $db = Typecho_Db::get();
+                $sql = $db->select()->from('table.comments')
+                    ->where('cid =?',$this->cid)
+                    ->where('mail =?', $this->remember('mail',true))
+                    ->where('status =?', 'approved')
+                    ->limit(1);
+                $result = $db->fetchAll($sql);
+                
+                $finalContent = getContentTest($this->content);  // 先保存原始内容
+                
+                if($this->user->hasLogin()) {
+                    // 处理登录可见
+                    $finalContent = preg_replace("/\[login\](.*?)\[\/login\]/sm",'<div class="login_reply2view jz jc ys">$1</div>', $finalContent);
+                } else {
+                    $finalContent = preg_replace("/\[login\](.*?)\[\/login\]/sm",'<div class="login_reply2view jz jc ys">此处内容需要<a href="/admin" target="_blank">登录</a>后方可阅读</div>', $finalContent);
+                }
+                
+                if($this->user->hasLogin() || $result) {
+                    // 处理回复可见
+                    $finalContent = preg_replace("/\[hide\](.*?)\[\/hide\]/sm",'<div class="reply2view jz jc ys">$1</div>', $finalContent);
+                } else {
+                    $finalContent = preg_replace("/\[hide\](.*?)\[\/hide\]/sm",'<div class="reply2view jz jc ys">此处内容需要<a href="#comments">评论</a>回复后（审核通过）方可阅读</div>', $finalContent);
+                }
+                
+                echo $finalContent;
+                ?>
+<!-- 回复&登录可见 -->
+
+                    
+    </div>
     </div>
 
  <!-- 评论占位区块 -->
