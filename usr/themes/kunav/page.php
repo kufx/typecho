3a1fa0e512fae8672b3f7c737026c9b58e5eeadf
kufx <144138627+kufx@users.noreply.github.com>
2025-04-19 13:02:47 +0800

    Update page.php

--- usr/themes/kunav/page.php
+++ usr/themes/kunav/page.php
@@ -10,7 +10,13 @@
 <?php echo getContentTest($this->content); ?>                </div>
     </div>
 
-
+ <!-- 评论占位区块 -->
+    <div class="category-group">
+        <div class="category-header">
+            <div class="main-category">用户评论</div>
+        </div>
+<?php $this->need('comments.php'); ?>
+    </div>
    
 <?php $this->need('js.php'); ?>
 <?php $this->need('footer.php'); ?>
