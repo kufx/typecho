6d5705613717c5fd7f1544fc495d135ec49ce601
kufx <144138627+kufx@users.noreply.github.com>
2025-04-20 14:24:12 +0800

    Update comments.php

--- usr/themes/kunav/comments.php
+++ usr/themes/kunav/comments.php
@@ -125,7 +125,7 @@ else{echo $str2;}
                 <?php $comments->cancelReply(); ?>
             </div>
 
-            <h3 id="response"><?php _e('添加新评论'); ?></h3>
+        
             <form method="post" action="<?php $this->commentUrl() ?>" id="comment-form" role="form">
                 <?php if ($this->user->hasLogin()): ?>
                     <p><?php _e('登录身份: '); ?><a
