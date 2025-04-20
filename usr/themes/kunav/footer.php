20583cd70841adea9569aabddd6885022bbe7d08
kufx <144138627+kufx@users.noreply.github.com>
2025-04-20 16:50:26 +0800

    Update footer.php

--- usr/themes/kunav/footer.php
+++ usr/themes/kunav/footer.php
@@ -2,11 +2,12 @@
 
     
 <footer id="footer" role="contentinfo">
-    &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
-    <?php _e('由 <a href="https://typecho.org">Typecho</a> 强力驱动'); ?>. <br>
     <?php if($this->options->footer): ?>
 <?php echo $this->options->footer(); ?>
-<?php endif; ?>
+<?php endif; ?><br>
+    &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
+    <?php _e('由 <a href="https://typecho.org">Typecho</a> 强力驱动'); ?>. 
+    
 </footer><!-- end #footer -->
 
 </body>
