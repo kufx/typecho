9f3c711f33fea03ca0a811de04198bf416a8a36a
kufx <144138627+kufx@users.noreply.github.com>
2025-04-21 19:03:00 +0800

    Update post.php

--- usr/themes/kunav/post.php
+++ usr/themes/kunav/post.php
@@ -76,16 +76,16 @@ $this->content = preg_replace_callback(
                 
                 if($this->user->hasLogin()) {
                     // 处理登录可见
-                    $finalContent = preg_replace("/\[login\](.*?)\[\/login\]/sm",'<div class="login_reply2view jz jc ys">$1</div>', $finalContent);
+                    $finalContent = preg_replace("/\[login\](.*?)\[\/login\]/sm",'<div class="login_reply2view">$1</div>', $finalContent);
                 } else {
-                    $finalContent = preg_replace("/\[login\](.*?)\[\/login\]/sm",'<div class="login_reply2view jz jc ys">此处内容需要<a href="/admin" target="_blank">登录</a>后方可阅读</div>', $finalContent);
+                    $finalContent = preg_replace("/\[login\](.*?)\[\/login\]/sm",'<div class="login_reply2view">此处内容需要<a href="/admin" target="_blank">登录</a>后方可阅读</div>', $finalContent);
                 }
                 
                 if($this->user->hasLogin() || $result) {
                     // 处理回复可见
-                    $finalContent = preg_replace("/\[hide\](.*?)\[\/hide\]/sm",'<div class="reply2view jz jc ys">$1</div>', $finalContent);
+                    $finalContent = preg_replace("/\[hide\](.*?)\[\/hide\]/sm",'<div class="reply2view open">$1</div>', $finalContent);
                 } else {
-                    $finalContent = preg_replace("/\[hide\](.*?)\[\/hide\]/sm",'<div class="reply2view jz jc ys">此处内容需要<a href="#comments">评论</a>回复后（审核通过）方可阅读</div>', $finalContent);
+                    $finalContent = preg_replace("/\[hide\](.*?)\[\/hide\]/sm",'<div class="reply2view lock">此处内容需要<a href="#comments">评论</a>回复后（审核通过）方可阅读</div>', $finalContent);
                 }
                 
                 echo $finalContent;
